40ff0b0ffb154dc43b0ee35b4e6ebe5a2a8835a4
Sam <sam@Kellys-MacBook-Air.local>
2016-09-07 16:22:13 -0400

    First commit

---
+++ src/hw02.java
@@ -0,0 +1,25 @@
+/**
+ * Created by reevamerchant on 9/7/16.
+ */
+public class hw02 {
+
+
+    private static double x = 12;
+    private static double y = 28;
+    private static double z = 3.3;
+
+
+    private static double calculate() {
+
+        return (x * y) / z;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("The answer to (x * y) / z is " + calculate());
+
+    }
+    }
+
+
